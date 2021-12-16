0e6711b9dc0917e7cd8014e24eb20e8db47b9325
Florent-Vanhollebeke <florent.vanhollebeke@gmail.com>
2021-12-16 22:56:21 +0100

    modification views, urls, models

--- django-web-app/merchex/listings/views.py
+++ django-web-app/merchex/listings/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def hello(request):
+    return HttpResponse('<h1>Hello Django!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>A propos</h1> <p>Nous adorons merch!</p>')
+
+
+def listings(request):
+    return HttpResponse('<h1>Liste des annonces pour les articles</h1><p>La liste arrive, patience!</p>')
+
+def contact(request):
+    return HttpResponse('<h1>Page de contact</h1><p>Nos coordonnées</p>')
+
