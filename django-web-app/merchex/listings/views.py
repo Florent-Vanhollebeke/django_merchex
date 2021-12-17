2f95339a03ad32babb33abba50ac02adb7262dec
Florent-Vanhollebeke <florent.vanhollebeke@gmail.com>
2021-12-17 23:34:21 +0100

    modification views, models, templates

--- django-web-app/merchex/listings/views.py
+++ django-web-app/merchex/listings/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Band, Listing
 
 def hello(request):
-    return HttpResponse('<h1>Hello Django!</h1>')
+    bands = Band.objects.all()
+    return render(request,'listings/hello.html')
 
 def about(request):
     return HttpResponse('<h1>A propos</h1> <p>Nous adorons merch!</p>')
 
-
 def listings(request):
-    return HttpResponse('<h1>Liste des annonces pour les articles</h1><p>La liste arrive, patience!</p>')
+    listings = Listing.objects.all()
+    return HttpResponse(f"""<h1>Liste des annonces pour les articles</h1> 
+                        <p>Ma petite liste!</p>
+                        <ul>
+                                <li>{listings[0].title}</li>
+                                <li>{listings[1].title}</li>
+                                <li>{listings[2].title}</li>
+                        </ul>""")
 
 def contact(request):
     return HttpResponse('<h1>Page de contact</h1><p>Nos coordonnées</p>')
